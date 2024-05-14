32ab2f129c9b13b617d868c3f9af50a060052d26
ryan5481 <ryan5481@me.com>
2024-05-14 12:56:57 +0800

    debugged deployment to live network

--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.19;
 
-import {DeployRaffle} from "../../script/DeployRaffle.sol";
+import {DeployRaffle} from "../../script/DeployRaffle.s.sol";
 import {Raffle} from "../../src/Raffle.sol";
 import {Test, console} from "forge-std/Test.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
